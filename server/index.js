5bf6df4cc749af4f93eced2ca8782ae787bb982e
Aviad Schiffmann <aviad.schiffmann@dynamicyield.com>
2021-10-15 10:35:53 +0300

    add local server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 80;
+const path = require('path');
+
+app.use('/', (...args) => {
+    let x = path.join(__dirname, '..');
+    express.static(x)(...args);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}); 
